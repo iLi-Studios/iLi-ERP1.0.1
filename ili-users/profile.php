96b5d50334f7acea4dce688071b522f984927fba
Sakly Ayoub <saklyayoub@live.com>
2015-10-26 13:56:56 +0100

    réecriture de page profile

--- ili-users/profile.php
+++ ili-users/profile.php
@@ -1,17 +1,13 @@
 <?php include"../ili-functions/functions.php";?>
-<?php autorisation('2'); ?>
 <?php include"../ili-functions/fragments/head.php";?>
 <!-- BEGIN BODY -->
 <body class="fixed-top">
-<!-- BEGIN HEADER -->
-<div id="header" class="navbar navbar-inverse navbar-fixed-top"> 
-<?php include"../ili-functions/fragments/top_navigation_bar.php";?>
-</div>
-<!-- END HEADER --> 
-<!-- BEGIN CONTAINER -->
-<div id="container" class="row-fluid"> 
-<?php include"../ili-functions/fragments/sidebar.php";?>
-	<!-- BEGIN PAGE -->  
+	<?php include"../ili-functions/fragments/page_header.php";?>
+	<!-- BEGIN CONTAINER -->
+	<div id="container" class="row-fluid">
+		<?php include"../ili-functions/fragments/sidebar.php";?>
+		<!-- BEGIN PAGE -->
+      <!-- BEGIN PAGE -->  
       <div id="main-content">
          <!-- BEGIN PAGE CONTAINER-->
          <div class="container-fluid">
@@ -241,34 +237,5 @@
       <!-- END PAGE -->  
    </div>
    <!-- END CONTAINER -->
-   <!-- BEGIN FOOTER -->
-   <div id="footer">
-       2013 &copy; Admin Lab Dashboard.
-      <div class="span pull-right">
-         <span class="go-top"><i class="icon-arrow-up"></i></span>
-      </div>
-   </div>
-   <!-- END FOOTER -->
-   <!-- BEGIN JAVASCRIPTS -->    
-   <!-- Load javascripts at bottom, this will reduce page load time -->
-   <script src="js/jquery-1.8.3.min.js"></script>
-   <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-   <script src="js/jquery.blockui.js"></script>
-   <!-- ie8 fixes -->
-   <!--[if lt IE 9]>
-   <script src="js/excanvas.js"></script>
-   <script src="js/respond.js"></script>
-   <![endif]-->
-   <script type="text/javascript" src="assets/chosen-bootstrap/chosen/chosen.jquery.min.js"></script>
-   <script type="text/javascript" src="assets/uniform/jquery.uniform.min.js"></script>
-   <script src="js/scripts.js"></script>
-   <script>
-      jQuery(document).ready(function() {       
-         // initiate layout and plugins
-         App.init();
-      });
-   </script>
-   <!-- END JAVASCRIPTS -->   
-</body>
-<!-- END BODY -->
-</html>
+<?php include"../ili-functions/fragments/footer.php";?>
+<script>jQuery(document).ready(function() {App.init();});</script>
