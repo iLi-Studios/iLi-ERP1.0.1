830de5e6d329aaba7151afcaa9fe1af7c33ac01d
Sakly Ayoub <saklyayoub@live.com>
2015-10-26 14:09:53 +0100

    HTML profile

--- ili-users/profile.php
+++ ili-users/profile.php
@@ -6,7 +6,6 @@
 <!-- BEGIN CONTAINER -->
 <div id="container" class="row-fluid">
 	<?php include"../ili-functions/fragments/sidebar.php";?>
-	<!-- BEGIN PAGE --> 
 	<!-- BEGIN PAGE -->
 	<div id="main-content"> 
 		<!-- BEGIN PAGE CONTAINER-->
@@ -14,15 +13,18 @@
 			<!-- BEGIN PAGE HEADER-->
 			<div class="row-fluid">
 				<div class="span12"> 
-					<!-- BEGIN THEME CUSTOMIZER-->
-					<div id="theme-change" class="hidden-phone"> <i class="icon-cogs"></i> <span class="settings"> <span class="text">Theme:</span> <span class="colors"> <span class="color-default" data-style="default"></span> <span class="color-gray" data-style="gray"></span> <span class="color-purple" data-style="purple"></span> <span class="color-navy-blue" data-style="navy-blue"></span> </span> </span> </div>
-					<!-- END THEME CUSTOMIZER--> 
 					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
-					<h3 class="page-title"> Profile <small>simple profile page</small> </h3>
+					<h3 class="page-title"> Profile <small> Gestion de Profile </small> </h3>
 					<ul class="breadcrumb">
-						<li> <a href="#"><i class="icon-home"></i></a><span class="divider">&nbsp;</span> </li>
-						<li> <a href="#">Extra</a> <span class="divider">&nbsp;</span> </li>
-						<li><a href="#">Profile</a><span class="divider-last">&nbsp;</span></li>
+						<li> <a href="<?php echo $site ; ?>"><i class="icon-home"></i></a><span class="divider">&nbsp;</span> </li>
+						<li><a href="<?php echo $site ; ?>">Profile</a><span class="divider-last">&nbsp;</span></li>
+						<li class="pull-right search-wrap">
+							<form class="hidden-phone">
+								<div class="search-input-area">
+									<input id=" " class="search-query" type="text" placeholder="Recherche ?">
+									<i class="icon-search"></i> </div>
+							</form>
+						</li>
 					</ul>
 					<!-- END PAGE TITLE & BREADCRUMB--> 
 				</div>
