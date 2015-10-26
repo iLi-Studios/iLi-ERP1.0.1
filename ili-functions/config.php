1984c7af8eed305d3be75f960467926f7606bda1
Sakly Ayoub <saklyayoub@live.com>
2015-10-26 13:27:26 +0100

    link change

--- ili-functions/config.php
+++ ili-functions/config.php
@@ -3,5 +3,5 @@ define('MYSQL_SERVEUR', 'localhost');
 define('MYSQL_UTILISATEUR', 'root');
 define('MYSQL_MOTDEPASSE', '');
 define('MYSQL_BASE', 'ili_erp');
-$site="http://localhost/ili-erp-beta/";
+$site="http://localhost/erp/";
 ?>
