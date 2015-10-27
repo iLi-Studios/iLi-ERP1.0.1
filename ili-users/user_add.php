c803136fff6913045289335d0ef869277744424b
Sakly Ayoub <saklyayoub@live.com>
2015-10-27 18:56:58 +0100

    codage HTML

--- ili-users/user_add.php
+++ ili-users/user_add.php
@@ -21,7 +21,7 @@ autorisation('5');
 				<ul class="breadcrumb">
 					<li> <a href="<?php echo $site ; ?>"><i class="icon-home"></i></a><span class="divider">&nbsp;</span> </li>
 					<li><a href="<?php echo $site ; ?>ili-users/users">Utilisateurs du système</a><span class="divider">&nbsp;</span></li>
-					<li><a href="<?php echo $site ; ?>ili-users/users_edit">Création</a><span class="divider-last">&nbsp;</span></li>
+					<li><a href="<?php echo $site ; ?>ili-users/user_add">Création</a><span class="divider-last">&nbsp;</span></li>
 					<li class="pull-right search-wrap">
 						<form class="hidden-phone">
 							<div class="search-input-area">
@@ -34,11 +34,74 @@ autorisation('5');
 			</div>
 		</div>
 		<!-- END PAGE HEADER--> 
-		<!-- BEGIN PAGE CONTENT-->
-		<div class="row-fluid">
-			<div class="span12">
 			
-			
+			<!-- BEGIN PAGE CONTENT-->
+            <div class="row-fluid">
+               <div class="span12">
+                  <div class="widget box blue" id="form_wizard_1">
+                     <div class="widget-title">
+                        <h4><i class="icon-reorder"></i> Création Utilisateur</h4>
+                        <span class="tools">
+                           <a href="javascript:;" class="icon-chevron-down"></a>
+                           <a href="javascript:;" class="icon-remove"></a>
+                        </span>
+                     </div>
+                     <div class="widget-body form">
+                        <form action="#" class="form-horizontal" method="post" enctype="multipart/form-data">
+							
+							<div class="control-group">
+								<label class="control-label">N° CIN</label>
+								<div class="controls">
+									<input type="text" required name="cin" class="span6  popovers" data-trigger="hover" data-content="Numéro de Carte Identité Nationnale" data-original-title="8 Chiffres" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Nom</label>
+								<div class="controls">
+									<input type="text" required name="nom" class="span6  popovers" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Prénom</label>
+								<div class="controls">
+									<input type="text" required name="prenom" class="span6  popovers" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Email</label>
+								<div class="controls">
+									<input type="email" required name="email" class="span6  popovers" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Tel</label>
+								<div class="controls">
+									<input type="text" required name="tel" class="span6  popovers" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Facebook URL</label>
+								<div class="controls">
+									<input type="text" required name="fb" class="span6  popovers" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">LinkedIN URL</label>
+								<div class="controls">
+									<input type="text" required name="linkedin" class="span6  popovers" />
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label">Github URL</label>
+								<div class="controls">
+									<input type="text" required name="github" class="span6  popovers" />
+								</div>
+							</div>
+							
+							
+                        </form>
+                     </div>
+                  </div>
 			
 			
 			</div>
