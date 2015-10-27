74dd8a2cf1e9b5a51d7db6a6913a3666d19396d6
Sakly Ayoub <saklyayoub@live.com>
2015-10-27 18:56:47 +0100

    création

--- ili-users/user_ban.php
+++ ili-users/user_ban.php
@@ -3,10 +3,10 @@
 autorisation('5'); 
 //$query2 = "SELECT * FROM users, users_diploma, users_expirance, users_rank, users_skills WHERE users.id_user=users_diploma.id_user AND users.id_user=users_expirance.id_user AND users.id_user=users_rank.id_user AND users.id_user=users_skills.id_user AND users.id_user='$id';";
 
-function user_remove($id){
-	$query="DELETE FROM `ili_erp`.`users_rank` WHERE `users_rank`.`id_user` = '$id';";
+function ban($id){
+	$query="UPDATE `ili_erp`.`users_rank` SET `id_rank` = '1' WHERE `users_rank`.`id_user` ='$id' ;";
 	if(query_execute("mysqli_fetch_object", $query)){return true;}
 }
-user_remove($_GET['id']);
+ban($_GET['id']);
 redirect('ili-users/users');
 ?>
