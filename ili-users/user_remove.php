ac7256aa96c19aa204ea81acaa1d893b6b1b2177
Sakly Ayoub <saklyayoub@live.com>
2015-10-28 11:05:59 +0100

    get error

--- ili-users/user_remove.php
+++ ili-users/user_remove.php
@@ -5,8 +5,8 @@ autorisation('5');
 
 function user_remove($id){
 	$query="DELETE FROM users WHERE id_user='$id';";
-	if(query_execute("mysqli_fetch_object", $query)){return '1';}else{return '0';}
+	if(query_execute("mysqli_fetch_object", $query)){return 1;}
 }
-if((user_remove($_GET['id'])) == '1'){redirect('ili-users/users?message=7');}
+if((user_remove($_GET['id']))==1){redirect('ili-users/users?message=7');}
 else{redirect('ili-users/users?message=6');}
 ?>
