185ed51d7cd78fe42aad37868ce193b0bda671ce
Sakly Ayoub <saklyayoub@live.com>
2015-10-29 16:32:51 +0100

    fix query

--- ili-users/user_ban.php
+++ ili-users/user_ban.php
@@ -4,7 +4,7 @@ autorisation('5');
 //$query2 = "SELECT * FROM users, users_diploma, users_expirance, users_rank, users_skills WHERE users.id_user=users_diploma.id_user AND users.id_user=users_expirance.id_user AND users.id_user=users_rank.id_user AND users.id_user=users_skills.id_user AND users.id_user='$id';";
 
 function ban($id){
-	$query="UPDATE `ili_erp`.`users_rank` SET `id_rank` = '1' WHERE `users_rank`.`id_user` ='$id' ;";
+	$query="UPDATE users SET id_rank='1' WHERE id_user='$id' ;";
 	if(query_execute("mysqli_fetch_object", $query)){return true;}
 }
 ban($_GET['id']);
