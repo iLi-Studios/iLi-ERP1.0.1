1cabbd34488bde96ec66737febbbfcc02c616fbe
Sakly Ayoub <saklyayoub@live.com>
2015-10-30 17:12:31 +0100

    Préparation de la forme modification user

--- ili-users/user_edit.php
+++ ili-users/user_edit.php
@@ -222,10 +222,51 @@ function get_users_diploma($id){
 		}
 	}
 }
-function age($date){return (int) ((time() - strtotime($date)) / 3600 / 24 / 365);}	
+function age($date){return (int) ((time() - strtotime($date)) / 3600 / 24 / 365);}
+function grade_list(){
+	//$query="SELECT * FROM `rank` WHERE `id_rank`>'1' AND `id_rank`<'6' ORDER BY id_rank ASC";
+	$query="SELECT * FROM `users_rank` ORDER BY id_rank ASC";
+	//if($_SESSION['user_id_rank']=6){$query==$query6;}else{$query==$query5;}
+	$result=query_excute_while($query);
+	while ($o=mysqli_fetch_object($result)){echo'<option value="'.$o->id_rank.'">'.$o->rank.'</option>';}
+}	
 ?>
 
-<?php include"../ili-functions/fragments/head.php";?>
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+<meta charset="utf-8" />
+<title>Form Components</title>
+<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+<meta content="" name="description" />
+<meta content="" name="author" />
+<link href="../ili-style/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link href="../ili-style/assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+<link href="../ili-style/assets/bootstrap/css/bootstrap-fileupload.css" rel="stylesheet" />
+<link href="../ili-style/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+<link href="../ili-style/css/style.css" rel="stylesheet" />
+<link href="../ili-style/css/style_responsive.css" rel="stylesheet" />
+<link href="../ili-style/css/style_default.css" rel="stylesheet" id="style_color" />
+<link href="../ili-style/assets/fancybox/source/jquery.fancybox.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/gritter/css/jquery.gritter.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/uniform/css/uniform.default.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/chosen-bootstrap/chosen/chosen.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/jquery-tags-input/jquery.tagsinput.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/clockface/css/clockface.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/bootstrap-wysihtml5/bootstrap-wysihtml5.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/bootstrap-datepicker/css/datepicker.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/bootstrap-timepicker/compiled/timepicker.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/bootstrap-colorpicker/css/colorpicker.css" />
+<link rel="stylesheet" href="../ili-style/assets/bootstrap-toggle-buttons/static/stylesheets/bootstrap-toggle-buttons.css" />
+<link rel="stylesheet" href="../ili-style/assets/data-tables/DT_bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="../ili-style/assets/bootstrap-daterangepicker/daterangepicker.css" />
+</head>
+<!-- END HEAD -->
 <!-- BEGIN BODY -->
 <body class="fixed-top">
 <?php include"../ili-functions/fragments/page_header.php";?>
@@ -288,7 +329,7 @@ function age($date){return (int) ((time() - strtotime($date)) / 3600 / 24 / 365)
 								</ul>
 							</div>
 							<div class="span6">
-								<h4><?php echo $user->nom; ?> <?php echo $user->prenom; ?><br/>
+								<h4><?php echo $user->nom; ?> <?php echo $user->prenom; ?> <span><a href="#myModal_info_mod" data-toggle="modal" class="icon-edit tooltips" data-original-title="Modifier les informations personnelles"></a></span><br/>
 									<small><?php echo $user->poste; ?></small></h4>
 								<table class="table table-borderless">
 									<tbody>
@@ -529,5 +570,91 @@ function age($date){return (int) ((time() - strtotime($date)) / 3600 / 24 / 365)
 		</div>
 	</div>
 </form><!-- End myModal_mdp_edit -->
-<?php include"../ili-functions/fragments/footer.php";?>
-<script>jQuery(document).ready(function() {App.init();});</script>
+<form action="" method="post">
+	<div id="myModal_info_mod" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModal_info_mod_Label" aria-hidden="true">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3 id="myModal_info_mod_Label">Modifier les informations personnelles</h3>
+		</div>
+		<div class="modal-body">
+			<center>
+				<table width="80%">
+					<tr>
+						<td width="40%">Nom</td>
+						<td width="60%"><input name="nom" required type="text" value="<?php echo $user->nom;?>" class="input-large" /></td>
+					</tr>
+					<tr>
+						<td>Prénom</td>
+						<td><input name="prenom" required type="text" value="<?php echo $user->prenom;?>" class="input-large" /></td>
+					</tr>
+					<tr>
+						<td>Poste</td>
+						<td><input name="poste" required type="text" value="<?php echo $user->poste;?>" class="input-large" /></td>
+					</tr>
+					<tr>
+						<td>Email</td>
+						<td><input name="email" required type="email" value="<?php echo $user->email;?>" class="input-large" /></td>
+					</tr>
+					<tr>
+						<td>Mobile</td>
+						<td><input name="tel" required type="text" value="<?php echo $user->tel;?>" class="input-large" /></td>
+					</tr>
+					<tr>
+						<td>Date de naissance</td>
+						<td><input name="tel" required type="date" value="<?php echo $user->date_naissance;?>" class="input-large" /></td>
+					</tr>
+					<tr>
+						<td>Grade</td>
+						<td><select name="rank" required tabindex="1"><?php grade_list(); ?></select></td>
+					</tr>
+				</table>
+			</center>
+		</div>
+		<div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Annuler</button>
+			<input type="submit" class="btn btn-primary" value="Mettre à jour ?"/>
+		</div>
+	</div>
+</form><!-- End myModal_info_mod -->
+
+<div id="footer"> 2013 &copy; Admin Lab Dashboard.
+	<div class="span pull-right"> <span class="go-top"><i class="icon-arrow-up"></i></span> </div>
+</div>
+<!-- END FOOTER --> 
+<!-- BEGIN JAVASCRIPTS --> 
+<!-- Load javascripts at bottom, this will reduce page load time --> 
+<script src="../ili-style/js/jquery-1.8.2.min.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/ckeditor/ckeditor.js"></script> 
+<script src="../ili-style/assets/bootstrap/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap/js/bootstrap-fileupload.js"></script> 
+<script src="../ili-style/js/jquery.blockui.js"></script> 
+<!-- ie8 fixes --> 
+<!--[if lt IE 9]>
+   <script src="js/excanvas.js"></script>
+   <script src="js/respond.js"></script>
+   <![endif]--> 
+<script type="text/javascript" src="../ili-style/assets/chosen-bootstrap/chosen/chosen.jquery.min.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/uniform/jquery.uniform.min.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-wysihtml5/wysihtml5-0.3.0.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-wysihtml5/bootstrap-wysihtml5.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/clockface/js/clockface.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/jquery-tags-input/jquery.tagsinput.min.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-toggle-buttons/static/js/jquery.toggle.buttons.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-datepicker/js/bootstrap-datepicker.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-daterangepicker/date.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-daterangepicker/daterangepicker.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-timepicker/js/bootstrap-timepicker.js"></script> 
+<script type="text/javascript" src="../ili-style/assets/bootstrap-inputmask/bootstrap-inputmask.min.js"></script> 
+<script src="../ili-style/assets/fancybox/source/jquery.fancybox.pack.js"></script> 
+<script src="../ili-style/js/scripts.js"></script> 
+<script>
+      jQuery(document).ready(function() {       
+         // initiate layout and plugins
+         App.init();
+      });
+   </script> 
+<!-- END JAVASCRIPTS -->
+</body>
+<!-- END BODY -->
+</html>
