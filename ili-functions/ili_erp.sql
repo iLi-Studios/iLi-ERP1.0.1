38262db06f467b5c84c815a05ed216c9f7e33297
Sakly Ayoub <saklyayoub@live.com>
2015-11-02 02:21:44 +0100

    MAJ

--- ili-functions/ili_erp.sql
+++ ili-functions/ili_erp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 28 Octobre 2015 à 11:27
+-- Généré le :  Lun 02 Novembre 2015 à 02:20
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -61,8 +61,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_by` varchar(200) COLLATE utf8_bin DEFAULT NULL,
   `created_date` date DEFAULT NULL,
   PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`),
   KEY `id_rank` (`id_rank`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id_user`, `id_rank`, `nom`, `prenom`, `email`, `poste`, `tel`, `adresse`, `date_naissance`, `mdp`, `mdp_update_date`, `fb`, `github`, `linkedin`, `img_link`, `created_by`, `created_date`) VALUES
+(8088718, 6, 'Sakly', 'Ayoub', 'sakly@ili-studios.com', 'CEO & Founder iLi-Studios SARL', '36166996', '19 Rue Ibn Zid AGBA 2010 MANOUBA TUNISIE', '1988-09-22', '21232f297a57a5a743894a0e4a801fc3', '2015-10-30', 'http://www.faecbook.com/saklyayoub', 'http://www.linkedin.com/', '', '', 'Sakly Ayoub', '2015-10-28'),
+(9188047, 4, 'Battikh', 'Aziz', 'battikh@ili-studios.com', 'Head Team Designer', '55836311', 'test', '10/30/2015', '140f6969d5213fd0ece03148e62e461e', '2015-10-30', '', '', '', '', 'Sakly Ayoub', '2015-10-30');
 
 -- --------------------------------------------------------
 
@@ -98,7 +107,15 @@ CREATE TABLE IF NOT EXISTS `users_expirance` (
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`),
   KEY `id_user_2` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Contenu de la table `users_expirance`
+--
+
+INSERT INTO `users_expirance` (`id`, `id_user`, `company`, `company_url`, `duration`, `experience`) VALUES
+(8, 8088718, 't1', 't2', 't3', 't4'),
+(9, 8088718, 'test1', 'test2', 'test3', 'test4');
 
 -- --------------------------------------------------------
 
@@ -112,6 +129,18 @@ CREATE TABLE IF NOT EXISTS `users_rank` (
   PRIMARY KEY (`id_rank`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Contenu de la table `users_rank`
+--
+
+INSERT INTO `users_rank` (`id_rank`, `rank`) VALUES
+(1, 'SUSPENDUE'),
+(2, 'UTILISATEUR GRADE 1'),
+(3, 'UTILISATEUR GRADE 2'),
+(4, 'UTILISATEUR GRADE 3'),
+(5, 'ADMIN'),
+(6, 'DEV');
+
 -- --------------------------------------------------------
 
 --
@@ -122,11 +151,20 @@ CREATE TABLE IF NOT EXISTS `users_skills` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `id_user` int(8) NOT NULL,
   `skills` varchar(200) COLLATE utf8_bin NOT NULL,
-  `pourcentage` int(2) NOT NULL,
+  `pourcentage` varchar(200) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`),
   KEY `id_user_2` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `users_skills`
+--
+
+INSERT INTO `users_skills` (`id`, `id_user`, `skills`, `pourcentage`) VALUES
+(1, 8088718, 'PHP', '84'),
+(2, 8088718, 'HTML5', '82'),
+(6, 8088718, 'C#', '81');
 
 --
 -- Contraintes pour les tables exportées
