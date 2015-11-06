0530baf40ad17dad636e933e865cf40a151fe6d0
Sakly Ayoub <saklyayoub@live.com>
2015-11-06 13:29:44 +0100

    update database

--- ili-functions/ili_erp.sql
+++ ili-functions/ili_erp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 02 Novembre 2015 à 10:47
+-- Généré le :  Ven 06 Novembre 2015 à 13:29
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -19,6 +19,51 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `ili_erp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `privileges_default`
+--
+
+CREATE TABLE IF NOT EXISTS `privileges_default` (
+  `id` int(8) NOT NULL,
+  `bloc` varchar(50) COLLATE utf8_bin NOT NULL,
+  `s` tinyint(1) NOT NULL,
+  `u` tinyint(1) NOT NULL,
+  `c` tinyint(1) NOT NULL,
+  `d` tinyint(1) NOT NULL,
+  `b` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `privileges_default`
+--
+
+INSERT INTO `privileges_default` (`id`, `bloc`, `s`, `u`, `c`, `d`, `b`) VALUES
+(1, 'users', 1, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `system_log`
+--
+
+CREATE TABLE IF NOT EXISTS `system_log` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_user` int(8) NOT NULL,
+  `date_operation` varchar(100) COLLATE utf8_bin NOT NULL,
+  `operation` varchar(500) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `system_log`
+--
+
+INSERT INTO `system_log` (`id`, `id_user`, `date_operation`, `operation`) VALUES
+(1, 8088718, '11/06/15-13:22:48', 'Connexion');
 
 -- --------------------------------------------------------
 
@@ -54,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id_user`, `id_rank`, `nom`, `prenom`, `email`, `poste`, `tel`, `adresse`, `date_naissance`, `mdp`, `mdp_update_date`, `fb`, `github`, `linkedin`, `img_link`, `created_by`, `created_date`) VALUES
-(8088718, 6, 'Sakly Sakly', 'Ayoub', 'sakly@ili-studios.com', 'CEO & Founder iLi-Studios SARL', '20666996', '19 Rue Ibn Zid AGBA 2010 MANOUBA TUNISIE', '1988-09-22', '3cb61b94f984497b9230075a6f777346', '2015-11-02', 'http://www.faecbook.com/saklyayoub', 'http://www.linkedin.com/', 'http://www.linkedin.com/', 'http://www.ili-studios.com/img/saklyayoub.png', 'Sakly Ayoub', '2015-10-28');
+(8088718, 3, 'Sakly', 'Ayoub', 'sakly@ili-studios.com', '					CEO & Founder iLi-Studios SARL', '20666996', '19 Rue Ibn Zid AGBA 2010 MANOUBA TUNISIE', '1988-09-22', '3cb61b94f984497b9230075a6f777346', '2015-11-02', 'http://www.faecbook.com/saklyayoub', 'http://www.linkedin.com/', 'http://www.github.com/', 'http://www.ili-studios.com/img/saklyayoub.png', 'Sakly Ayoub', '2015-10-28');
 
 -- --------------------------------------------------------
 
@@ -72,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `users_diploma` (
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`),
   KEY `id_user_2` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=10 ;
 
 --
 -- Contenu de la table `users_diploma`
@@ -84,7 +129,7 @@ INSERT INTO `users_diploma` (`id`, `id_user`, `annee`, `lieux`, `diplome`, `etab
 (4, 8088718, 2013, 'Tunis', 'BTS Developpement sur Internet', 'IMSET'),
 (5, 8088718, 2014, 'Kef', 'CEFEE', 'Bureau D''emploie'),
 (6, 8088718, 2014, 'Sousse', 'HP-Life E-learning', 'HP'),
-(7, 8088718, 2015, 'Manouba', 'Formation sur le projet <création d''une boite de développement>', 'API');
+(7, 8088718, 2015, 'Manouba', 'Formation sur le projet <création d''une boite de développement>', 'API Manouba');
 
 -- --------------------------------------------------------
 
@@ -102,7 +147,7 @@ CREATE TABLE IF NOT EXISTS `users_expirance` (
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`),
   KEY `id_user_2` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Contenu de la table `users_expirance`
@@ -112,6 +157,24 @@ INSERT INTO `users_expirance` (`id`, `id_user`, `company`, `company_url`, `durat
 (10, 8088718, 'WorkGroup', '', 'Jan. - Mars 2001', 'Developpeur Web WebMaster<br> •	Conception, Developpement, et realisation d’un site e-commerce pour une bijouterie, avec sont système de facturation.'),
 (11, 8088718, 'OkToBoot', 'http://www.oktoboot.com/', 'Sep. - Dec 2012', 'Developpeur Web<br>\r\n•	Conception, Developpement, et realisation d’un système ERP a base de Magento sur ZendFrimework\r\n'),
 (12, 8088718, 'Solution Interactives Visuelles', '', 'Jan. - Mars 2013', 'Developpeur Web, Administrateur Système<br>•	Conception, Developpement, et integration de système de gestion des serveurs de jeux avec un site de factorisation sur un serveur VPS a base de Linux\r\n•	Administration de serveur Linux, Creation de solution de backup automatisé.\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_privileges`
+--
+
+CREATE TABLE IF NOT EXISTS `users_privileges` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `id_user` int(8) NOT NULL,
+  `bloc` varchar(100) COLLATE utf8_bin NOT NULL,
+  `s` tinyint(1) NOT NULL,
+  `c` tinyint(1) NOT NULL,
+  `u` tinyint(1) NOT NULL,
+  `d` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -130,12 +193,9 @@ CREATE TABLE IF NOT EXISTS `users_rank` (
 --
 
 INSERT INTO `users_rank` (`id_rank`, `rank`) VALUES
-(1, 'SUSPENDUE'),
-(2, 'UTILISATEUR GRADE 1'),
-(3, 'UTILISATEUR GRADE 2'),
-(4, 'UTILISATEUR GRADE 3'),
-(5, 'ADMIN'),
-(6, 'DEV');
+(1, 'Suspendue'),
+(2, 'Utilisateur'),
+(3, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -151,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `users_skills` (
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`),
   KEY `id_user_2` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=34 ;
 
 --
 -- Contenu de la table `users_skills`
@@ -167,6 +227,12 @@ INSERT INTO `users_skills` (`id`, `id_user`, `skills`, `pourcentage`) VALUES
 --
 -- Contraintes pour les tables exportées
 --
+
+--
+-- Contraintes pour la table `system_log`
+--
+ALTER TABLE `system_log`
+  ADD CONSTRAINT `system_log_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 
 --
 -- Contraintes pour la table `users`
@@ -185,6 +251,12 @@ ALTER TABLE `users_diploma`
 --
 ALTER TABLE `users_expirance`
   ADD CONSTRAINT `users_expirance_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `users_privileges`
+--
+ALTER TABLE `users_privileges`
+  ADD CONSTRAINT `users_privileges_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `users_skills`
