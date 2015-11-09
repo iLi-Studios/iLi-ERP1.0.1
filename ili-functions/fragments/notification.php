1510609bab3a2adecebd35cfdd42ed3a549e6828
Sakly Ayoub <saklyayoub@live.com>
2015-11-09 14:35:25 +0100

    add notification module

---
+++ ili-functions/fragments/notification.php
@@ -0,0 +1,72 @@
+<!-- BEGIN NOTIFICATION DROPDOWN -->
+<?php
+//$link=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$id_user=$_SESSION['user_id'];
+function num_notif(){
+	$id_user=$_SESSION['user_id'];
+	$query="SELECT * FROM `system_notif` WHERE `id_user`='$id_user' AND `vu`='0'";
+	$o=query_execute("mysqli_num_rows", $query);
+	if($o=='0'){echo '';}
+	else{echo '<span class="badge badge-warning">'.$o.'</span>';}
+}
+function vu_notif($id){
+	$query="UPDATE `system_notif` SET `vu` = '1' WHERE `id` ='$id';";
+	query_execute_insert($query);
+}
+if(isset($_POST['vu'])){
+	$id=$_POST['vu'];
+	vu_notif($id);
+}
+function vu_tous_notif($id){
+	$query="UPDATE `system_notif` SET `vu` = '1' WHERE `id_user` ='$id';";
+	query_execute_insert($query);
+}
+if(isset($_POST['vu_tous'])){
+	$id=$_POST['vu_tous'];
+	vu_tous_notif($id);
+}
+function get_all_notification(){
+	$id_user=$_SESSION['user_id'];
+	$query="SELECT * FROM `system_notif` WHERE `id_user`='$id_user' AND `vu`='0' LIMIT 5";
+	$result=query_excute_while($query);
+	if(mysqli_num_rows($result)>'0'){
+		echo'<ul class="dropdown-menu extended notification">';
+	}
+	while ($o=mysqli_fetch_object($result)){
+		echo'
+			<li> 
+				<a href=""> '.$o->notification.' 
+					<span class="small italic" style="margin-left:4%"><br>'?><?php diff_date($o->date_notif); echo'
+						<br>
+						<form action="" method="post" style="margin-bottom: -3%;margin-top: -10%;">
+							<input type="submit" value="Vu?" style="text-decoration:none;margin-left:80%;line-height: 100%;border: 0;background: none;text-decoration: underline;" class="small italic"/>
+							<input type="hidden" name="vu" value="'.$o->id.'">
+						</form>
+					</span>
+				</a>
+			</li>				
+		';
+	}
+	if(mysqli_num_rows($result)>'0'){
+		echo'
+			<li>
+				<a href="#">
+					<form action="" method="post" style="margin-bottom: 2%;">
+						<input type="submit" value="Marquer tous comme Vus?" style="margin-left:10%;line-height: 100%;border: 0;background: none;text-decoration: underline;" class="small italic"/>
+						<input type="hidden" name="vu_tous" value="'.$id_user.'">
+					</form>
+				</a>
+			</li>';
+		echo'</ul>';
+	}
+}
+?>
+<li class="dropdown" id="header_notification_bar">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+		<i class="icon-bell-alt"></i>
+		<?php num_notif();?>
+	</a>
+		<?php get_all_notification();?>
+</li>
+<!-- END NOTIFICATION DROPDOWN -->
+
--- ili-functions/functions.php
+++ ili-functions/functions.php
@@ -30,10 +30,50 @@ function user_privileges($bloc, $id_user){
 	return $o;
 }
 function write_log($operation){
-	$now= date("m/d/y-H:i:s"); 
+	$now= date("d-m-y H:i:s"); 
 	$id_user=$_SESSION['user_id'];
 	$query="INSERT INTO `system_log` (`id`, `id_user`, `date_operation`, `operation`) VALUES (NULL, '$id_user', '$now', '$operation'); ";
 	query_execute_insert($query);
 }
-
+function diff_date($date){
+		if(!ctype_digit($date))
+			$date = strtotime($date);
+		
+		if(date('Ymd', $date) == date('Ymd')){
+			$diff = time()-$date;
+				
+			if($diff < 60) /* moins de 60 secondes */
+				echo 'Il y a '.$diff.' sec';
+			
+			else if($diff < 3600) /* moins d'une heure */
+				echo 'Il y a '.round($diff/60, 0).' minutes';
+			
+			else if($diff < 43200) /* moins de 12 heures */
+				echo 'Il y a '.round($diff/3600, 0).' heures' ;
+			
+			else /*  plus de 12 heures ont affiche ajourd'hui à HH:MM:SS */
+				echo 'Aujourd\'hui à '.date('H:i:s', $date);
+		}
+		
+			else if(date('Ymd', $date) == date('Ymd', strtotime('- 1 DAY')))
+				echo 'Hier à '.date('H:i:s', $date);
+			
+			else if(date('Ymd', $date) == date('Ymd', strtotime('- 2 DAY')))
+				echo 'Il y a 2 jours à '.date('H:i:s', $date);
+				
+			else if(date('Ymd', $date) == date('Ymd', strtotime('- 3 DAY')))
+				echo 'Il y a 3 jours à '.date('H:i:s', $date);
+				
+			else if(date('Ymd', $date) == date('Ymd', strtotime('- 4 DAY')))
+				echo 'Il y a 4 jours à '.date('H:i:s', $date);
+				
+			else if(date('Ymd', $date) == date('Ymd', strtotime('- 5 DAY')))
+				echo 'Il y a 5 jours à '.date('H:i:s', $date);
+			
+			else if(date('Ymd', $date) == date('Ymd', strtotime('- 6 DAY')))
+				echo 'Il y a 6 jours à '.date('H:i:s', $date);
+			
+			else
+				echo 'Le '.date('d/m/Y à H:i:s', $date);
+}
 ?>
